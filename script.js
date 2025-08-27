27cbdd5fbeff12cf33182b3129889a046456f000
GalladeFan475 <smashingsanjay6@gmail.com>
2025-08-27 16:54:32 +0800

    updated ui colours

--- script.js
+++ script.js
@@ -3,18 +3,15 @@ const dark = document.querySelector(".dark-button");
 const root = document.documentElement;
 
 dark.addEventListener("click", () => {
-  root.style.setProperty("--text-color", "rgba(235, 226, 226, 0.545)"),
+  root.style.setProperty("--text-color", "#f9fafb"),
     root.style.setProperty("--hf-background-color", "black"),
     root.style.setProperty("--form-color", "white"),
-    root.style.setProperty("--form-background-color", "rgb(108, 103, 103)");
+    root.style.setProperty("--form-background-color", "#5f6670");
 });
 
 light.addEventListener("click", () => {
   root.style.setProperty("--text-color", "black"),
-    root.style.setProperty(
-      "--hf-background-color",
-      "rgba(235, 226, 226, 0.545)"
-    ),
-    root.style.setProperty("--form-color", "rgb(108, 103, 103)"),
+    root.style.setProperty("--hf-background-color", "#f9fafb"),
+    root.style.setProperty("--form-color", "#5f6670"),
     root.style.setProperty("--form-background-color", "white");
 });
